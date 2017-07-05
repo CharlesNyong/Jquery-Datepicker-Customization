19717679bc6a5b378207b6fdd301c7fb7649eff4
Charleswils <asikpo@uwindsor.ca>
2017-07-05 10:35:56 -0400

    Constant file
    Stores all my constant variables

---
+++ constant.php
@@ -0,0 +1,30 @@
+<?php
+
+$arrMONTHS = array("Jan",
+				"Feb", 
+				"Mar",
+				"Apr",
+				"May",
+				"Jun",
+				"Jul",
+				"Aug",
+				"Dec",
+				"Oct",
+				"Nov",
+				"Sept");
+
+
+$arrMONTHS_MAP = array("Jan" => 0,
+				"Feb" => 1, 
+				"Mar" => 2,
+				"Apr" => 3,
+				"May" => 4,
+				"Jun" => 5,
+				"Jul" => 6,
+				"Aug" => 7,
+				"Dec" => 11,
+				"Oct" => 9,
+				"Nov" => 10,
+				"Sept" => 8);
+
+?>
